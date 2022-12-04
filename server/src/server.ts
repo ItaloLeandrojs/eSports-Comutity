b81a4e3a8a5e565802b8c79af4e5c695306f994c
Italo Leandro <italols.js@gmail.com>
2022-12-04 14:07:33 -0300

    update anuncio name

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.get('/ads', (request, response) => {
     return response.json([
-        {id: 1, name: "anuncio 1" },
+        {id: 1, name: "anuncio 1a" },
         {id: 2, name: "anuncio 2" },
         {id: 3, name: "anuncio 3" },
         {id: 4, name: "anuncio 4" }
