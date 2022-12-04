7b213474417c78d70390d5d932dc20f687a3bb6c
Italo Leandro <italols.js@gmail.com>
2022-12-04 13:33:57 -0300

    Aprimorando o Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import{MagnifyingGlassPlus} from 'phosphor-react';
+
 import './styles/main.css';
 
 import logoImg from './assets/logo-nlw-esports.svg'
@@ -21,7 +23,7 @@ function App(){
           </div>
         </a>
 
-        <a href="" className='relative'>
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-2.png" alt="" />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
@@ -30,7 +32,7 @@ function App(){
           </div>
         </a>
 
-        <a href="" className='relative'>
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-3.png" alt="" />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
@@ -39,7 +41,7 @@ function App(){
           </div>
         </a>
 
-        <a href="" className='relative'>
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-4.png" alt="" />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
@@ -48,7 +50,7 @@ function App(){
           </div>
         </a>
 
-        <a href="" className='relative'>
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game-5.png" alt="" />
 
           <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
@@ -65,6 +67,20 @@ function App(){
             <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
           </div>
         </a>
+      </div>
+
+      <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+            <strong className="text-2xl text-white font-black block">Não encontrou seu Duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anuncio para encontrar novos players!</span>
+         </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anuncio
+          </button>
+        </div>
       </div>
     </div>
   )
